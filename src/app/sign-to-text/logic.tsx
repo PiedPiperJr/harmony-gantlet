4f8b2e5367546d46bfa68ed94f15221aef362bb7
ZouhiMhd <hasszouma@gmail.com>
2025-01-29 19:27:32 +0100

    tensorflow working

--- src/app/sign-to-text/logic.tsx
+++ src/app/sign-to-text/logic.tsx
@@ -12,9 +12,9 @@ export async function loadModel() {
     }
 }
 
-export async function predictTf(model: tf.LayersModel, grid: number[][]) {
+export async function predictTf(model: tf.LayersModel, grid: number[][], alphabet:string[]) {
     if (!model) {
-        alert('Le modèle n\'est pas encore chargé');
+        console.warn('Le modèle n\'est pas encore chargé');
         return;
     }
 
@@ -22,19 +22,30 @@ export async function predictTf(model: tf.LayersModel, grid: number[][]) {
         // Convertir la grille en tensor 3D (1, 5, 5)
         const input = tf.tensor3d([grid]);
         console.log('Input tensor shape:', input.shape);
-        console.log('Input values:', await input.array());
 
         // Faire la prédiction
         let prediction = model.predict(input);
-        console.log('Prédiction brute:', prediction);
-
-        // Obtenir l'index de la classe prédite
-        // const predictedIndex = prediction.indexOf(Math.max(...prediction[0]));
-        // const predictedLetter = String.fromCharCode(65 + predictedIndex);
-
+        // Convertir le tenseur en tableau JavaScript
+        const predictionArray = await prediction.array();
+        
+        // Libérer la mémoire
         input.dispose();
+        prediction.dispose();
+        
+        // 
+        let letter = "0"
+        let min = 10000
+        
+        // predictionArray[0]
+        for (let i = 0; i< 26; i++) {
+            if (!isNaN(predictionArray[0][i]) && predictionArray[0][i] <= min) {
+                min = predictionArray[0][i]
+                letter  = alphabet[i]
+            }
 
-        return prediction;
+        }
+
+        return letter;
     } catch (error) {
         console.error('Erreur lors de la prédiction:', error);
         alert('Erreur lors de la prédiction');
